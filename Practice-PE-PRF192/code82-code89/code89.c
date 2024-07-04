7097f2c022accc763e5c17d4aba2b07f03bd90ac
Manabe0304 <hainamn833@gmail.com>
2024-07-04 23:23:47 +0700

    Update code89.c

--- Practice-PE-PRF192/code82-code89/code89.c
+++ Practice-PE-PRF192/code82-code89/code89.c
@@ -2,16 +2,27 @@
 #include <stdlib.h>
 
 int main()
-{system("cls");
- printf("\nTEST Q2 (2 marks):\n");
-  int i,n;double s;
-  printf("Enter n = "); scanf("%d",&n);
+{   system("cls");
+    printf("\nTEST Q2 (2 marks):\n");
+    int i, n;
+    double s = 0.0;
+    printf("Enter n = ");
+    scanf("%d", &n)
   //============================================================
   // Write your statements here
   
  
   //--------------------------------------------------------------------------------------------------------
-  printf("s = %.4f\n",s); 
+  // Calculate m based on the parity of n
+    int m = (n % 2 == 0) ? n : n - 1; //Toan tu ba ngoi
+    
+    // Calculate the sum s
+    for (i = 1; i <= m; i *= 2) {
+        s += 1.0 / i;
+    }
+
+    // Print the result
+    printf("s = %.4f\n", s);
 
   //====DO NOT ADD NEW OR CHANGE STATEMENTS AFTER THIS LINE====
   printf("\nOUTPUT:\n");
