f343cb246b8bc4145d9187774d3b52fc886736f6
Manabe0304 <hainamn833@gmail.com>
2024-07-12 09:56:04 +0700

    Update Example 5.c

--- Example slot 18/Example 5.c
+++ Example slot 18/Example 5.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+void sortDesc(int a[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (a[i] < a[j]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+
+int main(){
+    system("cls");
+    int n, i, a[n];
+    scanf("%d", &n);
+    if(n < 10){
+        for (i = 0; i < n; i++) scanf("%d", &a[i]);
+    }
+    int count = 0;
+    int s[n];
+    for (i = 0; i < n; i++){
+        if(a[i] % 2 == 1 && a[i] > 0){
+        s[count] = a[i] * a[i];
+        count++;
+        }
+    }
+    printf("\nOUTPUT\n");
+    if(count > 0) {
+        sortDesc(s, count);
+        for (i = 0; i < count; i++) {
+            printf("%d ", s[i]);
+        }
+        printf("\n");
+    } else {
+        return 1;
+    }
+    system("pause");
+    return 0;
+}
