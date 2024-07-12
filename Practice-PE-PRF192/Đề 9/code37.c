d625e4fafbb13f198842feec91db2acc39b61f71
Manabe0304 <hainamn833@gmail.com>
2024-07-12 08:44:22 +0700

    Update code37.c

--- Practice-PE-PRF192/Đề 9/code37.c
+++ Practice-PE-PRF192/Đề 9/code37.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
 int main(){
+    system ("cls");
 	int i, j, n;
 	scanf("%d", &n);
 	printf("\nOUTPUT:\n");
 	//i:1,2,3,...,n
 	for(i = 1;i <= n; i++){
 		//j:1,2,3,...,n+1-i
-		for(j = 1; j <= n; j++){
-			if(i == n || j == n || n - i == j - 1 ){
+		for(j = 1; j <= n; j++)
+			if(i == n || j == n || n - i == j - 1 )
 				printf("*");
-				}
 			else
 				printf(" ");
 		}
-		printf("\n");
-	}
+	printf("\n");
+	system("pause");
 	return 0;
 }
