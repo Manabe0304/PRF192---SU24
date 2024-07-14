7d61d04bea50273bbe1c14f9e493b75a49904551
Manabe0304 <hainamn833@gmail.com>
2024-07-15 03:28:38 +0700

    Update Q7.c

--- PE-PRF192-SU24/Đề 6/Q7/Q7.c
+++ PE-PRF192-SU24/Đề 6/Q7/Q7.c
@@ -10,27 +10,17 @@ int main() {
   //INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
   int i, j, n;
   scanf("%d", &n);
-  double k, array[n];
+  double k, arr[n];
   for(i = 0; i < n; i++) {
-        scanf("%lf", &array[i]);
+        scanf("%lf", &arr[i]);
   }
   scanf("%lf", &k);
-  
-  double filteredArray[n];
-  int filteredSize = 0;
-  for(i = 0; i < n; i++) {
-        if(array[i] != k) {
-            filteredArray[filteredSize] = array[i];
-            filteredSize++;
-        }
-    }
-
-  for(i = 0; i < filteredSize - 1; i++) {
-        for(j = i + 1; j < filteredSize; j++) {
-            if(filteredArray[i] < filteredArray[j]) {
-                double temp = filteredArray[i];
-                filteredArray[i] = filteredArray[j];
-                filteredArray[j] = temp;
+  for (i = 0; i < n-1; i++) {
+        for (j = i+1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                double temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
@@ -38,8 +28,10 @@ int main() {
   // Fixed Do not edit anything here.
   printf("\nOUTPUT:\n");
   //@STUDENT: WRITE YOUR OUTPUT HERE:
-  for(i = 0; i < filteredSize; i++) {
-        printf("%.1lf ", filteredArray[i]);
+  for (i = 0; i < n; i++) {
+        if (arr[i] != k) {
+            printf("%.1f ", arr[i]);
+        }
     }
 
   //--FIXED PART - DO NOT EDIT ANY THINGS HERE
