fa9a03103fe80855c0089add3c40a9725063b9ef
Manabe0304 <hainamn833@gmail.com>
2024-07-15 02:57:29 +0700

    Create Q7.c

---
+++ PE-PRF192-SU24/Đề 4/Q7/Q7.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+
+int main() {
+  system("cls");
+  //INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
+  int n, k, i, j, temp;
+  printf("Enter n = ");
+  scanf("%d", &n);
+  int array[n];
+  printf("Enter array = ");
+  for (i = 0; i < n; i++) {
+    scanf("%d", &array[i]);
+  }
+  printf("Enter k = ");
+  scanf("%d", &k);
+
+  for (i = 0; i < n - 1; i++) {
+    for (j = i + 1; j < n; j++) {
+      if (array[i] > array[j]) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+
+  // Fixed Do not edit anything here.
+  printf("\nOUTPUT:\n");
+  //@STUDENT: WRITE YOUR OUTPUT HERE:
+  for (i = 0; i < n; i++) {
+    if (array[i] != k) {
+      printf("%d", array[i]);
+    }
+  }
+  
+  //--FIXED PART - DO NOT EDIT ANY THINGS HERE
+  printf("\n");
+  system ("pause");
+  return(0);
+}
