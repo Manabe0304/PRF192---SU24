fb3d7ce2098e9ef548e926d0b3213fea6e480018
Manabe0304 <hainamn833@gmail.com>
2024-07-15 02:48:39 +0700

    Create Q2.c

---
+++ PE-PRF192-SU24/Đề 4/Q2/Q2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+
+int main() {
+  system("cls");
+  //INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
+  int m, n, temp;
+  int i; 
+  double sum = 0; 
+  scanf("%d %d", &m, &n);
+
+  if (m > n) {
+    temp = m;
+    m = n;
+    n = temp;
+  }
+
+  // Fixed Do not edit anything here.
+  printf("\nOUTPUT:\n");
+  //@STUDENT: WRITE YOUR OUTPUT HERE:
+  for (i = m; i <= n; i++) {
+    sum += i;
+  }
+  printf("%.3lf", sum / (n-m+1));
+
+  //--FIXED PART - DO NOT EDIT ANY THINGS HERE
+  printf("\n");
+  system ("pause");
+  return(0);
+}
