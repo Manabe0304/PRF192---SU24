f510a090747edf224627e86626e0973f960eb8a6
Manabe0304 <hainamn833@gmail.com>
2024-07-15 10:40:11 +0700

    Create Q3.c

---
+++ PE-PRF192-SU24/Đề 8/Q3/Q3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+
+int main() {
+  system("cls");
+  //INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
+    int n, i, count = 0;
+    scanf("%d", &n);
+  
+  // Fixed Do not edit anything here.
+  printf("\nOUTPUT:\n");
+  //@STUDENT: WRITE YOUR OUTPUT HERE:
+    for (i = 1; i <= 100; i++) {
+        if (i % n == 0) {
+            count++;
+        }
+    }
+    printf("%d", count);
+  
+    if (count == 0) {
+         printf("There is no number divisible by %d\n", n);
+    }
+ 
+  //--FIXED PART - DO NOT EDIT ANY THINGS HERE
+  printf("\n");
+  system ("pause");
+  return(0);
+}
