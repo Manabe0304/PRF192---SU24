c5fa336930d47fbcb499919e24d8bfdc9afcc3cf
Manabe0304 <hainamn833@gmail.com>
2024-07-18 09:25:00 +0700

    Update Q9.c

--- PE-PRF192-SU24/Đề 6/Q9/Q9.c
+++ PE-PRF192-SU24/Đề 6/Q9/Q9.c
@@ -1,3 +1,5 @@
+//Cach 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -34,5 +36,48 @@ int main() {
   //--FIXED PART - DO NOT EDIT ANY THINGS HERE
   printf("\n");
   system ("pause");
+  return(0);
+}
+
+//Cach 2
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+
+int main() {
+  //INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
+  int i, j;
+  char s1[1000], s2[100], s3[100];
+  scanf(" %[^\n]s", s1);
+  scanf("%s", s2);
+  scanf("%s", s3);
+  int ls1 = strlen(s1);
+  int ls2 = strlen(s2);
+  int ls3 = strlen(s3);
+  
+  for (i = 0; i <= ls1 - ls2; ){
+      if(strncmp(&s1[i], s2, ls2) == 0){
+          for (j = 0; j < ls3; j++){
+              s1[i+j] = s3[j];
+          }
+          for (j = i + ls2; j <= ls1; j++){
+              s1[j] = s1[j + ls2 - ls3];
+          }
+          ls1 -= ls2 - ls3;
+      } else {
+          i++;
+      }  
+  }
+
+  // Fixed Do not edit anything here.
+  printf("\nOUTPUT:\n");
+  //@STUDENT: WRITE YOUR OUTPUT HERE:
+  printf("%s", s1);
+  
+  //--FIXED PART - DO NOT EDIT ANY THINGS HERE
+  printf("\n");
   return(0);
 }
